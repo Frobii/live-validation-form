55fbdc772b969fd6e22798b6e5973b036071bc1d
Frobii <frobi.webdev@outlook.com>
2023-07-27 15:21:24 +0930

    setup-initial-api-validation-for-the-form-and-email

--- script.js
+++ script.js
@@ -15,9 +15,34 @@ const formConfiguration = () => {
     });
   }
 
+  function validateEmail() {
+    const emailField = document.getElementById('email');
+    emailField.required = true;
+
+    emailField.addEventListener('input', (event) => {
+      if (emailField.validity.valid) {
+        console.log('valid')
+      } else {console.log('invalid')}
+    })
+  }
+
+  function liveValidation() {
+    validateEmail()
+
+    const submitButton = document.querySelector('.submit-button');
+    const form = document.getElementById('signupForm')
+
+    submitButton.addEventListener('click', (event) => {
+      event.preventDefault();
+      form.reportValidity();
+    })
+  }
+
   return {
     setFormButton,
+    liveValidation,
   };
 };
 
 formConfiguration().setFormButton();
+formConfiguration().liveValidation();
